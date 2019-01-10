bfc7a0ff7ba2be46da9240cc04df89ed7fe3f663
bartoszgaj <bartosz.gaj4@gmail.com>
2019-01-10 01:06:31 +0100

    Added frame on map view

--- src/Presentation/Tile.java
+++ src/Presentation/Tile.java
@@ -8,6 +8,8 @@ import javafx.scene.shape.Rectangle;
 
 public class Tile extends StackPane {
 
+    private static final int AREA_SIZE = 100;
+
     private static final int TILE_SIZE = 3;
     public int x;
     public int y;
@@ -32,34 +34,39 @@ public class Tile extends StackPane {
         getChildren().add(square);
     }
 
-    public void setFill(double oilLevel, Type type){
-        switch (type) {
-            case COAST:
-                square.setFill(this.coast);
-                break;
-            case LAND:
-                square.setFill(this.land);
-                break;
-            case OIL:
-                square.setFill(this.oilColor);
-                if (oilLevel < 1.0E-14) {
+    public void setFill(double oilLevel, Type type) {
+        if (x == 0 || y == 0 || y == AREA_SIZE-1 || x == AREA_SIZE-1) {
+            square.setFill(Color.BLACK);
+        } else {
+
+            switch (type) {
+                case COAST:
+                    square.setFill(this.coast);
+                    break;
+                case LAND:
+                    square.setFill(this.land);
+                    break;
+                case OIL:
+                    square.setFill(this.oilColor);
+                    if (oilLevel < 1.0E-14) {
+                        square.setFill(this.water);
+                    } else if (oilLevel < 1.0E-11) {
+                        square.setFill(this.oilColor4);
+                    } else if (oilLevel < 1.0E-8) {
+                        square.setFill(this.oilColor3);
+                    } else if (oilLevel < 1.0E-5) {
+                        square.setFill(this.oilColor2);
+                    } else if (oilLevel < 1.0E-2) {
+                        square.setFill(this.oilColor1);
+                    }
+                    break;
+                case SOURCE:
+                    square.setFill(source);
+                    break;
+                case WATER:
                     square.setFill(this.water);
-                } else if (oilLevel < 1.0E-11) {
-                    square.setFill(this.oilColor4);
-                } else if (oilLevel < 1.0E-8) {
-                    square.setFill(this.oilColor3);
-                } else if (oilLevel < 1.0E-5) {
-                    square.setFill(this.oilColor2);
-                } else if (oilLevel < 1.0E-2) {
-                    square.setFill(this.oilColor1);
-                }
-                break;
-            case SOURCE:
-                square.setFill(source);
-                break;
-            case WATER:
-                square.setFill(this.water);
-                break;
+                    break;
+            }
         }
     }
 }
